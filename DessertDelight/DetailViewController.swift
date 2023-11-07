2ce43892e0a07cda928bbaf7f68992ba74a5a9c2
Anup Deshpande <deshpande.anup24@gmail.com>
2023-11-07 11:31:20 -0800

    Add detailViewController

---
+++ DessertDelight/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  DessertDelight
+//
+//  Created by Anup Deshpande on 11/7/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
